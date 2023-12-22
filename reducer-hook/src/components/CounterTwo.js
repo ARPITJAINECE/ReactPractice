9b227a7d9d0060b7fb6238b79b27460fdc21828f
unknown <aj241997@gmail.com>
2023-12-22 16:47:41 +0530

    react reducer

---
+++ reducer-hook/src/components/CounterTwo.js
@@ -0,0 +1,15 @@
+//in this illustration, we will be using the the state (currentState) object 
+//and the action object, instead of using the state and the object directly 
+//as we did in the previous example(CounterOne.js)
+
+import React from 'react'
+
+function CounterTwo() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default CounterTwo
