a6e47077000305bb8e8a6b32174f3b4e0c28c51f
unknown <aj241997@gmail.com>
2024-01-11 00:45:00 +0530

    ref hook react

--- ref-hook/src/App.js
+++ ref-hook/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FocusInput from './components/FocusInput';
 import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className="App">
       <FocusInput></FocusInput>
       <ClassTimer></ClassTimer>
+      <HookTimer></HookTimer>
     </div>
   );
 }
